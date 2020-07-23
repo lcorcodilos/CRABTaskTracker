192ddbcfe95461d3c7f0e484d2dfdea90439ad44
lcorcodilos <lcorcodilos@users.noreply.github.com>
2020-07-22 22:18:09 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="CRABTaskTracker",
+    version="1.0",
+    author = "Lucas Corcodilos",
+    author_email = "corcodilos.lucas@gmail.com",
+    license = "gpl-3.0",
+    keywords = "crab3 task track",
+    url = "https://github.com/lcorcodilos/CRABTaskTracker",
+    long_description=read('README'),
+)
